9425b20353519ef014316be1c9af777bd5b077e8
paulpan13 <paulpanagioto14@gmail.com>
2026-05-20 19:58:34 +0300

    Adding DNN

---
+++ nj0pt2.js
@@ -0,0 +1,5 @@
+////TMVA Classification macro for H->μμ analysis
+//This macro implements the signal(Higgs) vs backgournd(Z boson)
+//discrimination for the zero-jet channel (nj=0), following the ATLAS analysis methodology.
+//MLP_vs_BDT_vs_DNN. The two methods will be tested and evaluated for their performance.
+//Vol.2 Adding DNN.
